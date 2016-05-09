4e29cb433e10a5df1207f63cf8b533836d7bb644
hy451176411 <451176411@qq.com>
2016-05-09 22:13:40 +0800

    主页面的tableview

--- Itserv_oa/ShedHomeViewController.h
+++ Itserv_oa/ShedHomeViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ShedHomeViewController : UIViewController
-
+@interface ShedHomeViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *mTableView;
+@property (weak, nonatomic) NSArray *titleArray;//第一层列表需要展示的数据
 @end
